adccc03751d78efc4e6d10ea25353c83c354c69a
Gregory Shulov <gregory.shulov@gmail.com>
2015-01-02 07:17:55 +0200

    Fixes #34: Modify quick installation scripts

--- scripts/htpc-server.sh
+++ scripts/htpc-server.sh
@@ -1,11 +1,13 @@
-echo "\n\033[0;32m >> Install Git and Ansible\033[0m"
+echo -e "\n\033[0;32m >> Install Git and Ansible\033[0m"
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo /usr/bin/apt-get -y install git ansible
 if [ ! -d "$HOME/htpc-ansible" ]; then
-	echo "\n\033[0;32m >> Clone htpc-ansible repository\033[0m"
+	echo -e  "\n\033[0;32m >> Clone htpc-ansible repository\033[0m"
 	git clone git://github.com/GR360RY/htpc-ansible.git "$HOME/htpc-ansible"
 else
-	echo "\n\033[0;32m >> htpc-ansible is already available\033[0m"
+	echo -e "\n\033[0;32m >> htpc-ansible is already available\033[0m"
 fi
 cd "$HOME/htpc-ansible"
-echo "\n\033[0;32m >> Install HTPC Server with Bittorent and Usenet Downloaders\033[0m"
+echo -e "\n\033[0;32m >> Install HTPC Server with Bittorent and Usenet Downloaders\033[0m"
 ansible-playbook -i hosts -c local -K htpc-server.yml
