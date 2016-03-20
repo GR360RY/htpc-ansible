afeb7d12c6cece6e42a63e800d7aeb4aba26dcf3
Gregory Shulov <gregory.shulov@gmail.com>
2016-03-20 20:26:33 +0200

    Remove quickinstall wizard

--- scripts/quickinstall.sh
+++ scripts/quickinstall.sh
@@ -1,7 +1,9 @@
 echo -e "\n\033[0;32m >> Install Requirements\033[0m"
 sudo apt-get -y update 2>&1 >/dev/null
 sudo /usr/bin/apt-get -y install git python-pip
-sudo /usr/bin/pip install -U ansible prompter
+# sudo /usr/bin/pip install -U ansible prompter
+sudo /usr/bin/pip install -U ansible
+
 
 if [ ! -d "$HOME/htpc-ansible" ]; then
 	echo -e  "\n\033[0;32m >> Clone htpc-ansible repository\033[0m"
@@ -10,7 +12,7 @@ else
 	echo -e "\n\033[0;32m >> htpc-ansible is already available\033[0m"
 fi
 cd "$HOME/htpc-ansible"
-echo -e "\n\033[0;32m >> Run Wizard\033[0m"
-python scripts/wizard.py <&1
+# echo -e "\n\033[0;32m >> Run Wizard\033[0m"
+# python scripts/wizard.py <&1
 echo -e "\n\033[0;32m >> Installing ...\033[0m"
 ansible-playbook -i hosts -c local -K htpc-server.yml
